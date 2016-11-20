faa50410120bef194970b2d177250a00634eaf90
Micheál <micheal.boland@ucdconnect.ie>
2016-11-20 21:50:58 +0000

    20/11/2016: Final version

--- sparseSOR.py
+++ sparseSOR.py
@@ -14,8 +14,32 @@ import numpy as np
 import math
 import sys
 import copy
+from sparseSORModule import vect_norm, csr_store, sparse_sor
+import matplotlib.pyplot as plt
 
-## FUNCTION DEFINITIONS ##############################
+
+### FUNCTION DEFINITIONS ##############################
+
+def fname_prompt():
+    ## This function prompts for user input for desired input or output filename
+    # User prompted for output filename. If no value entered default nas_Sor.in selected.    
+    ip_name = input("Please enter full input filename including extension: ")
+    if ip_name == "":
+        ip_name = "nas_Sor.in"
+        print("Default ", ip_name, " selected as input filename")
+    else:
+        print(ip_name, " selected as input filename")
+    
+    # User prompted for output filename. If no value entered default nas_Sor.out selected.    
+    op_name = input("Please enter full output filename including extension: ")
+    if op_name == "":
+        op_name = "nas_Sor.out"
+        print("Default ",op_name, " selected as output filename")
+    else: 
+        print(op_name, " selected as output filename")
+        
+    return {'ip_name':ip_name,'op_name':op_name}
+
 # This takes in filename that has matrix stored in required format and stores it in compressed format
 def csr_store(file_name):
     val = []
@@ -34,34 +58,20 @@ def csr_store(file_name):
                 rowSum = 0
                 
                 for j in range(n):
-                    
-#                    if (i==j) and float(mat_temp[i].strip().split("\t")[j]) == 0:
-#                        diagZero = 0
-#                    elif (i==j) and float(mat_temp[i].strip().split("\t")[j]) != 0:
-#                        diagZero = 1
-#                        diagEntry = float(mat_temp[i].strip().split("\t")[j])
-#                        colSum[j] -= diagEntry
                         
                     if float(mat_temp[i].strip().split("\t")[j]) != 0:
                         
                         startCheck += 1
-                        #colSum[j] += float(mat_temp[i].strip().split("\t")[j])
-                        #rowSum += float(mat_temp[i].strip().split("\t")[j])
-                        
                         val.append(float(mat_temp[i].strip().split("\t")[j]))
                         col.append(j)
                         
                         if startCheck == 1:
                             rowStart.append(len(val)-1)
                 
-                #print(i , ":",rowSum)
-                #print(i, ":", diagEntry - (rowSum - diagEntry))
-                
             else:
                 # b read in as a column vector            
                 b[0:,0] = mat_temp[i].strip().split("\t")
         
-        #print(colSum)
         rowStart.append(len(val))
         return {'val':val, 'col':col, 'rowStart':rowStart, 'b':b}  
         
@@ -130,44 +140,32 @@ def sparse_sor(A,b,maxits,e,w):
 
 ## END FUNCTION DEFINITIONS ############################
 
-## This section prompts for user input for desired input or output filename
-# User prompted for output filename. If no value entered default nas_Sor.in selected.    
-ip_name = input("Please enter full input filename including extension: ")
-if ip_name == "":
-    ip_name = "nas_Sor.in"
-    print("Default ", ip_name, " selected as input filename")
-else:
-    print(ip_name, " selected as input filename")
+# Prompting user for filename input
+fnames = fname_prompt()
 
-# User prompted for output filename. If no value entered default nas_Sor.out selected.    
-op_name = input("Please enter full output filename including extension: ")
-if op_name == "":
-    op_name = "nas_Sor.out"
-    print("Default ",op_name, " selected as output filename")
-else:
-    print(op_name, " selected as output filename")
-##
-
-A = csr_store1(ip_name)
+# Storage of A in CSR format
+A = csr_store(fnames['ip_name'])
 print("\nINPUT MATRICES:")
 print("A in CSR format")
 print("val:",A['val'])
 print("col:",A['col'])
 print("rowStart:",A['rowStart'])
+
 print("b COLUMN MATRIX:")
 b = A['b']
 print(b)
 
 #SparseSOR input parameters
-maxIts = 100
+maxIts = 10000
 e = 10**-5
-result = sparse_sor(A,b,maxIts,e,1.25)
+w = 1.25
+result = sparse_sor(A,b,maxIts,e,w)
 
-## Writing to nas_Sor.out
-headings = ['Stopping reason','Max num of iterations','Number of iterations','Macine epsilon','X seq tolerance']
+# Creating lists for headers and content for .out file
+headings = ['Stopping reason','Max num of iterations','Number of iterations','Machine epsilon','X seq tolerance']
 content = [result['stopReason'],str(maxIts),str(result['numIts']),str(sys.float_info.epsilon),str(e)]
 
-# This section will append traling spaces where necessary to align in output file
+# This section will append traling spaces where necessary to align headers with content in output file
 for i in range(len(headings)):
     if len(headings[i])>len(content[i]):
         content[i] = content[i].ljust(len(headings[i]))
@@ -175,7 +173,7 @@ for i in range(len(headings)):
         headings[i] = headings[i].ljust(len(content[i]))
 
 # Writing to output file
-with open(op_name,'w') as outfile:
+with open(fnames['op_name'],'w') as outfile:
     for entry in headings:    
         outfile.write(str(entry)+"\t")
     outfile.write('\n')
@@ -186,82 +184,28 @@ with open(op_name,'w') as outfile:
         for entry in result['x']:
             outfile.write(str(entry)+"\t")
 
-print("Output saved to: ", op_name)
+print("Output saved to: ", fnames['op_name'])
 
-######## TRASH #################################
-
-##-------------------
-##-------------------
-## 4/11/2016 This section has the SOR working for small matrices---------
+# Plotting tolerance versus number of iterations. Uncomment when required
+#maxIts = 10000
+#e = [10**-1,10**-2,10**-3,10**-4,10**-5,10**-6,10**-7,10**-8]
 #
-#def SOR(A,b,w,maxits):
+## Collecting values for iterations for defined tolerances
+#for i in range(len(e)):
+#    iterations.append(sparse_sor(A,b,maxIts,e[i],1.25)['numIts'])
 #
-#    x = np.zeros_like(b)
-#    
-#    for i in range(maxits):
-#        
-#        x_k = np.zeros_like(x)
-#        
-#        for j in range(A.shape[0]): #Returning length of first row of A
-#            s1 = np.dot(A[j,:j],x_k[:j])
-#            s2 = np.dot(A[j,j + 0:],x[j + 0:])
-#            x_k[j] = x[j] + (b[j] - s1 - s2)*(w/A[j,j])
-#            
-#        x = x_k
-#    
-#    return x
-#
-#A = np.matrix([[3.0, -1.0, 1.0],[-1.0, 3.0, -1.0],[1.0, -1.0, 3.0]])
-#b = np.array([-1.0,7.0,-7.0])
-#w = 1.25
-#maxits = 100
-#    
-##print(SOR(A,b,w,maxits))
-
-#-----------------
-
-# Using example from notes
-#B = np.matrix([[21,0,0,12,0,0],
-#               [0,1,49,0,0,0],
-#                [31,16,65,0,0,23],
-#                [0,0,0,85,0,0],
-#                [55,0,0,0,42,0],
-#                [0,0,0,0,0,41]])
-#
-#csr = csr_store(B)            
-#print('val = ',csr['val'])
-#print('col = ',csr['col'])
-#print('rowStart = ',csr['rowStart'])
-## This of course returning CSR with convention that first element is 0 rather than 1. Python implementation
-#def csr_store(B):
-#    # Obtain size of matrix. Cycling through each row    
-#    val = []
-#    col = []
-#    rowStart = []    
-#    for i in range(B.shape[0]):
-#        # Set this to zero at start of each new row. Used to populate rowStart vector        
-#        startCheck = 0
-#        
-#        # Iterating through each column
-#        for j in range(B.shape[0]):
-#            
-#            # Ignore zero entries
-#            if B[i,j] != 0:
-#                
-#                # When first entry in a row reached iterate check value
-#                startCheck += 1
-#                # Appending relevant values to val and col vectors
-#                val.append(B[i,j])
-#                col.append(j)
-#                
-#                # Here if startCheck is 1 then new rowStart value needs to be appended
-#                if startCheck == 1:
-#                    rowStart.append(len(val)-1)
-#                    
-#    # Finally append the last value to row start, namely the length of the val vector
-#    rowStart.append(len(val))
-#    # Output is returned as a dictionary that can be referenced by the various different key values    
-#    return{'val':val, 'col':col, 'rowStart':rowStart}
+## Plotting of result
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.set_ylim(-0.01,0.11)
+#plt.plot(iterations,e,'r-')
+#plt.plot(iterations,e,'ro')
+#for i,j in zip(iterations,e):
+#    ax.annotate(("numIts="+str(i)+"\n"+"Tolerance="+str(j)),xy=(i,j))
+#plt.xlabel("Tolerance")
+#plt.ylabel("Number of Iterations")
+#plt.title("Tolerance vs Number of Iterations")
+#plt.show()
 
 
     
